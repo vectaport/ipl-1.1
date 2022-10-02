cac38e38145578999a90965ed7c65b6eb3073d31
Scott Johnston <johnston@vectaport.com>
2022-10-02 08:27:15 -0700

    build on ivtools-2.0.11

--- src/ipledit_/main.c
+++ src/ipledit_/main.c
@@ -35,6 +35,8 @@
 #include <ipl/version.h>
 using std::cerr;
 
+#include <execinfo.h>
+
 /*****************************************************************************/
 
 static PropertyData properties[] = {
@@ -159,7 +161,24 @@ static OptionDesc options[] = {
 
 /*****************************************************************************/
 
+void stack_trace_handler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+ 
 int main (int argc, char** argv) {
+  
+   signal(SIGSEGV, stack_trace_handler);
+  
 #ifdef HAVE_ACE
     Dispatcher::instance(new AceDispatcher(ComterpHandler::reactor_singleton()));
 #endif
